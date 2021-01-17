8715730320c09ab794fc3224ae99c1411fb5d714
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-01-18 01:51:02 +0300

    Course updates

---
+++ OutKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OutKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1;
+            int number2 = 100;
+
+            int result = Add(out number1, number2);
+
+            Console.WriteLine(result);
+            Console.WriteLine(number1);
+        }
+
+        static int Add(out int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+    }
+}
---
+++ RefKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RefKeyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 20;
+            int number2 = 100;
+
+            int result = Add(ref number1, number2);
+
+            Console.WriteLine(result);
+            Console.WriteLine(number1);
+        }
+
+        static int Add(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+        }
+    }
+}
