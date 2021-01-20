bdbf2403ef8e19b0e0ca521f013f1ebc4ea77d78
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-01-20 22:30:12 +0300

    OOP1 added

---
+++ OOP1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager pm = new ProductManager();
+
+            Product product1 = new Product()
+            {
+                Id = 1,
+                Name = "Elma",
+                Price = 10,
+                Description = "Yeşil Elma"
+            };
+
+            pm.Add(product1);
+            Console.WriteLine(product1.Name);
+        }
+    }
+}
