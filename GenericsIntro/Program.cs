01fc0aee3348959d018cb7d56391063693fb6245
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2021-01-22 09:17:11 +0300

    Generics - List<> replica

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -6,7 +6,21 @@ namespace GenericsIntro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MyList<string> myList = new MyList<string>();
+
+            Console.WriteLine(myList.Length);
+
+            myList.Add("Anan");
+            myList.Add("Bacın");
+            myList.Add("Gardaşın");
+
+            Console.WriteLine(myList.Length);
+
+            foreach (string item in myList.GetItems)
+            {
+                Console.WriteLine("---------------------");
+                Console.WriteLine(item);
+            }
         }
     }
 }
